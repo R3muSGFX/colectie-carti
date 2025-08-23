04b916d265695c09a69523926f4199c25b11f27c
Remus Moisi <remus.r3mus@gmail.com>
2025-08-23 14:47:27 +0300

    adaug configurare initiala

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    ripple: true,
+    inputStyle: 'filled',
+    theme: Aura,
+    zIndex: {
+        modal: 1100,        //dialog, drawer
+        overlay: 1000,      //select, popover
+        menu: 1000,         //overlay menus
+        tooltip: 1100       //tooltip
+    }
+});
+app.mount('#app');
